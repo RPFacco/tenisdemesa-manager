4d43c658d779180449fd2d9b7b84cebaba13bf3e
RPFacco <arpfacco@gmail.com>
2026-07-07 12:05:51 -0300

    add thymeleaf-layout-dialect dependency

---
+++ src/main/java/com/ana/tenisdemesa/TenisdemesaApplication.java
@@ -0,0 +1,13 @@
+package com.ana.tenisdemesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisdemesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisdemesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ana/tenisdemesa/TenisdemesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ana.tenisdemesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisdemesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
